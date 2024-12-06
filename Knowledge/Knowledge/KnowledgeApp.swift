3d7fb4eb50e21d243a9dbd6ea5e3cd8297125ec8
Francesco Silvestro <francesco.silvestro03@gmail.com>
2024-12-06 12:05:23 +0100

    Add existing project

---
+++ Knowledge/Knowledge/KnowledgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnowledgeApp.swift
+//  Knowledge
+//
+//  Created by Francesco Silvestro on 06/12/24.
+//
+
+import SwiftUI
+
+@main
+struct KnowledgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
