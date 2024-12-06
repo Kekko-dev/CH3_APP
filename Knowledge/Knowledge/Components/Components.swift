569b7282113d51519fdf7ab8828b94d2c8770fae
Francesco Silvestro <francesco.silvestro03@gmail.com>
2024-12-06 13:01:29 +0100

    Added the button view of the modifiers

---
+++ Knowledge/Knowledge/Components/Components.swift
@@ -0,0 +1,18 @@
+//
+//  Components.swift
+//  Knowledge
+//
+//  Created by Francesco Silvestro on 06/12/24.
+//
+
+import SwiftUI
+
+struct Components: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Components()
+}
