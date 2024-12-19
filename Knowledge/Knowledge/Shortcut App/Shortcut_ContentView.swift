6173371b86bffd86bf239b135cfa3fc3bb7a68f7
Kekko-dev <francesco.silvestro03@gmail.com>
2024-12-19 18:40:44 +0100

    Added all of the HIG components, (just the text for a clear vision of what I need to do)

--- Knowledge/Knowledge/Shortcut App/Shortcut_ContentView.swift
+++ Knowledge/Knowledge/Shortcut App/Shortcut_ContentView.swift
@@ -13,8 +13,38 @@ struct Shortcut_ContentView: View {
         NavigationStack{
             ScrollView{
                 VStack{
+                    
+                    //CONTENT
                     HStack{
-                        Text("Inputs")
+                        Text("Content")
+                            .font(.title)
+                            .fontWeight(.heavy)
+                            .padding()
+                        Spacer()
+                    }
+                    ScrollView(.horizontal){
+                        HStack{
+                            Text("Content components")
+                        }
+                    }
+                    
+                    //Layout and Organization
+                    HStack{
+                        Text("Layout and Organization")
+                            .font(.title)
+                            .fontWeight(.heavy)
+                            .padding()
+                        Spacer()
+                    }
+                    ScrollView(.horizontal){
+                        HStack{
+                            Text("Layout and organization components")
+                        }
+                    }
+                    
+                    //MENUS AND ACTIONS
+                    HStack{
+                        Text("Menus and Actions")
                             .font(.title)
                             .fontWeight(.heavy)
                             .padding()
@@ -29,15 +59,92 @@ struct Shortcut_ContentView: View {
                                     
                                     
                             }
-                            NavigationLink(destination: Pickers()){
-                                ShortcutCardView(icon: "rectangle.on.rectangle.angled", title: "Picker", color: .red)
-                                    .padding(.bottom)
-                                    .padding(.horizontal)
-                            }
+                           
                         }
                     }
+                    
+                    //NAVIGATION AND SEARCH
+                    HStack{
+                        Text("Navigation and Search")
+                            .font(.title)
+                            .fontWeight(.heavy)
+                            .padding()
+                        Spacer()
+                    }
+                    ScrollView(.horizontal){
+                        HStack{
+                            Text("Navigation and search components")
+                        }
+                    }
+                    
+                    //PRESENTATION
+                    HStack{
+                        Text("Presentation")
+                            .font(.title)
+                            .fontWeight(.heavy)
+                            .padding()
+                        Spacer()
+                    }
+                    ScrollView(.horizontal){
+                        HStack{
+                            Text("Presentation components")
+                        }
+                    }
+                    
+                    
+
+                    //SELECTION AND INPUTS
+                    HStack{
+                        Text("Selection and Inputs")
+                            .font(.title)
+                            .fontWeight(.heavy)
+                            .padding()
+                        Spacer()
+                    }
+                        ScrollView(.horizontal){
+                            HStack{
+                                NavigationLink(destination: Pickers()){
+                                    ShortcutCardView(icon: "rectangle.on.rectangle.angled", title: "Picker", color: .red)
+                                        .padding(.bottom)
+                                        .padding(.horizontal)
+                                }
+                            }
+                        }
+                    
+                    
+                    
+                    //STATUS
+                    HStack{
+                        Text("Status")
+                            .font(.title)
+                            .fontWeight(.heavy)
+                            .padding()
+                        Spacer()
+                    }
+                    ScrollView(.horizontal){
+                        HStack{
+                            Text("Status components")
+                        }
+                    }
+                    
+                    
+                    //SYSTEM EXPERIENCE
+                    HStack{
+                        Text("System Experience")
+                            .font(.title)
+                            .fontWeight(.heavy)
+                            .padding()
+                        Spacer()
+                    }
+                    ScrollView(.horizontal){
+                        HStack{
+                            Text("System Experience components")
+                        }
+                    }
+                    
+                    }
                 }
-            }
+            
         }
         .navigationTitle("Components")
     }
