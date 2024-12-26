66c633cde5033827e7e47047b06fbc15d41c9161
Kekko-dev <francesco.silvestro03@gmail.com>
2024-12-26 11:32:20 +0100

    Added the content components file

---
+++ Knowledge/Knowledge/Components/Content/Text Views.swift
@@ -0,0 +1,18 @@
+//
+//  Text Views.swift
+//  LumiCode
+//
+//  Created by Francesco Silvestro on 26/12/24.
+//
+
+import SwiftUI
+
+struct Text_Views: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Text_Views()
+}
---
+++ Knowledge/Knowledge/Components/Content/Web Views.swift
@@ -0,0 +1,18 @@
+//
+//  Web Views.swift
+//  LumiCode
+//
+//  Created by Francesco Silvestro on 26/12/24.
+//
+
+import SwiftUI
+
+struct Web_Views: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Web_Views()
+}
