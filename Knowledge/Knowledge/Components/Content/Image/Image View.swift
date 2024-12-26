fed6ae30b8cec02e23bfb3997dbe104ab7c306ac
Kekko-dev <francesco.silvestro03@gmail.com>
2024-12-26 13:01:18 +0100

    Added image view to be fixed

---
+++ Knowledge/Knowledge/Components/Content/Image/Image View.swift
@@ -0,0 +1,18 @@
+//
+//  Image View.swift
+//  LumiCode
+//
+//  Created by Francesco Silvestro on 26/12/24.
+//
+
+import SwiftUI
+
+struct Image_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Image_View()
+}
